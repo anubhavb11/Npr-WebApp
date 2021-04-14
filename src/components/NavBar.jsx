aa54165516fe9f7698969382044e4bfaa875b9a1
Anubhav Bansal <anubhavb11@gmail.com>
2021-04-14 14:13:12 +0530

    Updated navbar added dropdown options

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Navbar,Nav,Form,FormControl,Button }from 'react-bootstrap';
+import {Navbar,Nav,NavDropdown }from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class NavBar extends React.Component {
@@ -10,16 +10,29 @@ export default class NavBar extends React.Component {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Inventory</Nav.Link>
-        <Nav.Link href="#sales">Sales/Out Challan</Nav.Link>
+      
+
+        <NavDropdown title="Inventory" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">View Inventory</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">View Summary</NavDropdown.Item>
+        </NavDropdown>
+
+        <NavDropdown title="Sales/Out Challan" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">View Challans</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Verify Challans</NavDropdown.Item>
+        </NavDropdown>
+
         <Nav.Link href="#clent">Client/Customer</Nav.Link>
         <Nav.Link href="#user">User</Nav.Link>
         <Nav.Link href="#po">PO</Nav.Link>
         <Nav.Link href="#payment">Payment</Nav.Link>
+
+      
       </Nav>
     </Navbar.Collapse>
   </Navbar>
       )
   }
 }
+
 
