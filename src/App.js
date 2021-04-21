f591d2f412b398d9a36b054853ca37b8d9f77f6e
Anubhav Bansal <anubhavb11@gmail.com>
2021-04-21 15:41:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar'
+
 import SignIn from './components/Login'
-import Header from './components/header'
+
 import Sidebar from './components/sidebar'
 // import './custom.scss'
 import ViewInventory from './components/ViewInventory'
